0eb20544e35714342765ab4bd1bed673bb2305de
joehua87 <joehua87@gmail.com>
2017-12-28 22:05:38 +0700

    export createRenderNode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // @flow
 
+import createRenderNode from './createRenderNode'
 import { getNodeData, getNodeType } from './getNodeInfo'
 import getEditProps from './getEditProps'
 import getTheme from './getTheme'
 import getComponentData from './getComponentData'
 
 export {
+  createRenderNode,
   getNodeData,
   getNodeType,
   getEditProps,
