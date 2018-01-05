2727340f5b73c2f0902849d7fa1c82d9e1dda247
joehua87 <joehua87@gmail.com>
2018-01-05 19:01:59 +0700

    data should not return null

--- src/getNodeInfo.js
+++ src/getNodeInfo.js
@@ -2,7 +2,7 @@
 
 export function getNodeData(value: TValue, key: string) {
   const node = value.document.getNode(key)
-  return node && node.data && node.data.toJSON()
+  return (node && node.data && node.data.toJSON()) || {}
 }
 
 export function getNodeType(value: TValue, key: string) {
